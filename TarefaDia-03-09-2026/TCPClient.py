285c9521a68e0d023a4d63579ea78d3f30f10853
Késsia Silva <kessiasilvadealmeida@gmail.com>
2026-09-03 10:51:58 -0300

    Initial commit

---
+++ TarefaDia-03-09-2026/TCPClient.py
@@ -0,0 +1,51 @@
+# Késsia e Ana Paula
+import socket
+import time
+# Função principal do cliente
+#Jogo: Advinhe o valor - usuario - servidor em que o 
+#servidor vai mandar uma operação e o cliente irá responder
+def start_client():
+    server_address = '127.0.0.1'
+    server_port = 8000
+
+    # Criação do socket TCP e conexão com o servidor
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((server_address, server_port))
+        print("[INFO] Conectado ao servidor.")
+        a = "nao"
+
+        while a == "nao":
+
+            # Servidor envia a operação
+            data = client_socket.recv(40).decode()
+            print(f"{data}")
+            data = client_socket.recv(40).decode()
+            print(f"Resolva: {data}")
+
+            #Usuario digita a resposta
+            resposta = input("Resposta: ")
+            client_socket.send(resposta.encode())
+            
+
+                # Aguarda a resposta do servidor
+            resultado = client_socket.recv(40).decode()
+            print(f"Recebido: {resultado}")
+                
+            # Espera 2 segundos antes de enviar o próximo pedido
+            print(f"Quer sair? digite: sim")
+            a = input("")
+
+
+            time.sleep(2)
+
+# Início da execução
+if __name__ == "__main__":
+    start_client()
+
+
+
+  
+
+
+
+
---
+++ TarefaDia-03-09-2026/TCPServer.py
@@ -0,0 +1,84 @@
+# Késsia e Ana Paula
+import socket
+import threading
+import random
+
+
+# Classe que representa uma thread para lidar com cada cliente
+class ClientThread(threading.Thread):
+    def __init__(self, client_socket, address):
+        threading.Thread.__init__(self)
+        self.client_socket = client_socket
+        self.address = address
+        print(f"[INFO] Conectado a {address}")
+
+    def run(self):
+        try:
+            # Inicio de jogo - Mensagem
+            response = f"Bem vindo ao jogo da calculadora"
+            self.client_socket.send(response.encode())
+
+            while True:
+                # Usuario escolheu iniciar
+                operacoes = ['+', '-', '*', '/']
+                a = random.randint(1, 100)
+                b = random.randint(1, 100)
+                op = random.choice(operacoes)
+                response = f"{a} {op} {b} ="
+                self.client_socket.send(response.encode())
+
+
+
+                # Recebe a resposta do cliente
+                resposta = self.client_socket.recv(40).decode()
+
+                # Calcula o resultado correto
+                if op == '+':
+                  resultado = a + b
+
+                elif op == '-':
+                  resultado = a - b
+
+                elif op == '*':
+                  resultado = a * b
+
+                elif op == '/':
+                  resultado = a / b
+
+                if not resposta:
+                    break
+                # Compara a resposta do usuário com o resultado correto
+                if str(resultado).lower()== resposta.lower():
+                   print("Resposta correta!")
+                   self.client_socket.send("Correto!".encode())
+                else:
+                   print("Resposta incorreta!")
+                   self.client_socket.send(
+                   f"Errado! A resposta correta é {resultado}".encode())
+    
+                
+                
+        except Exception as e:
+            print(f"[ERRO] {e}")
+        finally:
+            self.client_socket.close()
+            print(f"[INFO] Conexão com {self.address} encerrada.")
+
+# Função principal do servidor
+def start_server():
+    server_port = 8000
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', server_port))
+    server_socket.listen()
+
+    print(f"[INFO] Servidor escutando na porta {server_port}...")
+
+    # Loop principal para aceitar conexões de clientes
+    while True:
+        client_socket, addr = server_socket.accept()
+        thread = ClientThread(client_socket, addr)
+        thread.start()
+
+# Início da execução
+if __name__ == "__main__":
+    start_server()
